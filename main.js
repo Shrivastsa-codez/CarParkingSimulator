c30f03016585d49857423b44a7b89aaa02152684
Shrivastsa-codez <78452805+Shrivastsa-codez@users.noreply.github.com>
2021-11-21 12:45:25 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-canvas = document.getElementById('my canvas');
+canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
 greencar_width = 75;
@@ -77,7 +77,7 @@ function up()
 
 function down()
 {
-	if(greencar_y<=0){
+	if(greencar_y<=500){
 		greencar_y=greencar_y+10;
 		console.log("When down arrow is pressed, x="+ greencar_x + "|y="+ greencar_y);
 		uploadBackground();
@@ -97,7 +97,7 @@ function left()
 
 function right()
 {
-	if(greencar_x<=0){
+	if(greencar_x<=700){
 		greencar_x=greencar_x+10;
 		console.log("When right arrow is pressed, x="+ greencar_x + "|y="+ greencar_y);
 		uploadBackground();
